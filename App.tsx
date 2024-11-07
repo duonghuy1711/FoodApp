b0a05a0e5a2df4a5f1ec3e69392835b9fb23886b
Dương Quốc Huy <90316396+duonghuy1711@users.noreply.github.com>
2024-11-07 20:24:13 +0700

    Bài 2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //string 
+  const [name, setName] = useState("Dương_Huy");
+
+  //number
+  const [age, setAge] = useState(0);
+
+  //null, undefined, boolean
+
+  //object, array
+  const [person, setPerson] = useState({
+    name: "Dương_Huy",
+    age: 25,
+    address: {
+      street: "123 Main St",
+      city: "New York"
+    }
+  });
+
   return (
     <View style={styles.container}>
+      <View>
+        <Text>{name}</Text>
+        <Text>{JSON.stringify(person)}</Text>
+      </View>
       <Text>hello word Duong Huy</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -14,7 +35,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
